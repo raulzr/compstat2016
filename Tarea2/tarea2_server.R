bb12895296ff860bc46c145aa29bf5215e1de238
rzrkal <rej108@hotmail.com>
2016-10-26 18:39:11 -0500

    Quitar comentarios extras

--- Tarea2/tarea2_server.R
+++ Tarea2/tarea2_server.R
@@ -7,10 +7,6 @@ tarea2 <- function(input, output, session) {
   })
   
   output$Grafica <- renderPlot({
-    # x <- seq(input$valA,input$valB,length.out = 100)
-    # y <- sapply(x,fun1())
-    # plot(x,y,type="l",col="blue",main="Grafica 1" )
-    #hist(runif(input$n))
     val_Int <- c()
     val_std <- c()
     Ns <- c(10,100,1000,10000,100000)
@@ -23,13 +19,9 @@ tarea2 <- function(input, output, session) {
       val_std <- c(val_std, desv_std)
     }
     
-    #df <- data.frame(x = rep(log10(Ns),3), y = c(val_Int,qnorm(1-input$Int_conf, mean=val_Int, sd=val_std),qnorm(input$Int_conf, mean=val_Int, sd=val_std)))
     df <- data.frame(log10_N =log10(Ns), med = val_Int, infe = qnorm(1-input$Int_conf, mean=val_Int, sd=val_std), sup = qnorm(input$Int_conf, mean=val_Int, sd=val_std))
     ggplot(df) + geom_line(aes(log10_N,med), color = "red") + geom_line(aes(log10_N,infe), color = "blue") + geom_line(aes(log10_N,sup), color = "blue") + labs(x="10^n simulaciones", y="Valor esperado")
-    #ggplot(df, aes(x, y)) + geom_line(color="blue") +geom_point()
-    #plot(log10(Ns), val_Int, main = "Valor a N numero de iteraciones", type="b")#, color = "blue")
-    
-    
+
   })
   
    output$Resultado <- renderText({
